defe8833419e923aad9fee3e19be62f11c9a94c4
DoomedBunny <aflinton@hotmail.co.uk>
2020-02-02 19:00:18 +0000

    add cors

--- server/functions/express-graphql/app.js
+++ server/functions/express-graphql/app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
+const cors = require("cors");
 
 const app = express();
+
+// allow cross-origin requests
+app.use(cors());
 
 app.use(
   "/",
