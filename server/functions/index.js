8aa4ad54a16bf52a07ff682a26a2d1c68eeb97f6
DoomedBunny <aflinton@hotmail.co.uk>
2020-02-02 11:32:15 +0000

    remove unused imports from index.js

--- server/functions/index.js
+++ server/functions/index.js
@@ -1,6 +1,4 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const myServer = require('./express-graphql/app');
 
 const app = require('./express-graphql/app');
 
